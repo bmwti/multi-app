b02d2a53a239be8bd7fe00e7a9b35f7f9683f9a0
Renan Theodoro <renan.b.theodoro@gmail.com>
2017-10-05 22:42:37 -0300

    criando hackathons

--- projeto/src/app/app.routes.ts
+++ projeto/src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import {Routes} from '@angular/router'
 
 import {HomeComponent} from './home/home.component'
+import {HackathonListagemComponent} from './hackathon-listagem/hackathon-listagem.component';
+import {HackathonSingleComponent} from './hackathon-single/hackathon-single.component';
 import {BlogListagemComponent} from './blog-listagem/blog-listagem.component';
 import {BlogSingleComponent} from './blog-single/blog-single.component';
 import {PerfilComponent} from './perfil/perfil.component';
 
 export const ROUTES: Routes = [
   {path:'', component: HomeComponent},
+  {path:'hackathons', component: HackathonListagemComponent},
+  {path:'internet-das-coisas-para-todos', component: HackathonSingleComponent},
   {path:'blog', component: BlogListagemComponent},
   {path:'o-que-e-hackathon', component: BlogSingleComponent},
   {path:'user', component: PerfilComponent}
