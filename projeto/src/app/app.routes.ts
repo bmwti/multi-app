00a31cfccaa0f3648bb6ef63d2696a2b3f1506cc
logonrm <logonrm@fiap.com.br>
2017-10-05 22:39:00 -0300

    Ajuste blog

--- projeto/src/app/app.routes.ts
+++ projeto/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import {HomeComponent} from './home/home.component'
 import {BlogListagemComponent} from './blog-listagem/blog-listagem.component';
 import {BlogSingleComponent} from './blog-single/blog-single.component';
 import {PerfilComponent} from './perfil/perfil.component';
+import {LoginComponent} from './login/login.component';
 
 export const ROUTES: Routes = [
   {path:'', component: HomeComponent},
   {path:'blog', component: BlogListagemComponent},
   {path:'o-que-e-hackathon', component: BlogSingleComponent},
-  {path:'user', component: PerfilComponent}
+  {path:'user', component: PerfilComponent},
+  {path:'login', component: LoginComponent}
 ]
