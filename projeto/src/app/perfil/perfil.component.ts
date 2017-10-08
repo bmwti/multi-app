bc9626d823190c5f4e07d42954117885afd1f54d
Renan Theodoro <renan.b.theodoro@gmail.com>
2017-10-08 16:35:27 -0300

    listagem de moedas

--- projeto/src/app/perfil/perfil.component.ts
+++ projeto/src/app/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-perfil',
@@ -10,6 +11,17 @@ export class PerfilComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var abrirModal = function() {
+      $('.modal, .nosso-overlay').fadeIn();
+    }
+
+    var fecharModal = function() {
+      $('.modal, .nosso-overlay').fadeOut();
+    }
+
+    $('.openModal').click(abrirModal);
+    $('.nosso-overlay').click(fecharModal);
+    $('.btn-fechar').click(fecharModal);
   }
 
 }
